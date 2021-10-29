34960284a0f2d78968e57478bdf12384725df22e
emreisler <emre.isler.9@gmail.com>
2021-10-29 17:41:27 +0300

    gelistirme

--- modules/stock-service/src/main/java/com/example/emailservice/StockServiceApplication.java
+++ modules/stock-service/src/main/java/com/example/emailservice/StockServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmailServiceApplication {
+public class StockServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmailServiceApplication.class, args);
+		SpringApplication.run(StockServiceApplication.class, args);
 	}
 
 }
